699230f689bdcd5ef7bb9f14be5254acf39018f3
atylerh <athagstr@asu.edu>
2018-04-27 11:36:54 -0700

    Google places api

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,8 +2,27 @@ import React, {Component} from 'react'
 import './Navbar.css';
 import { Responsive, Button, Menu, Input, Dropdown } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
+import PlacesAutocomplete from 'react-places-autocomplete'
+import { geocodeByAddress, geocodeByPlaceId, getLatLng } from 'react-places-autocomplete'
+
 
 class Navbar extends Component {
+
+  constructor(props) {
+    super(props);  
+    this.state = { address: '' }
+  }
+
+  handleChange = (address) => {
+    this.setState({ address })
+  }
+
+  handleSelect = (address) => {
+    geocodeByAddress(address)
+      .then(results => getLatLng(results[0]))
+      .then(latLng => console.log('Success', latLng))
+      .catch(error => console.error('Error', error))
+  }
 
   render() {
 
@@ -18,7 +37,36 @@ class Navbar extends Component {
           </Responsive>
           
         <Menu.Item>
-          <Input className='SearchBar' icon='search' placeholder='Park near...' />
+          <PlacesAutocomplete
+        value={this.state.address}
+        onChange={this.handleChange}
+        onSelect={this.handleSelect}
+      >
+        {({ getInputProps, suggestions, getSuggestionItemProps }) => (
+          <div>
+            <input
+              {...getInputProps({
+                placeholder: 'Search Places ...',
+                className: 'location-search-input'
+              })}
+            />
+            <div className="autocomplete-dropdown-container">
+              {suggestions.map(suggestion => {
+                const className = suggestion.active ? 'suggestion-item--active' : 'suggestion-item';
+                // inline style for demonstration purpose
+                const style = suggestion.active
+                            ? { backgroundColor: '#fafafa', cursor: 'pointer' }
+                            : { backgroundColor: '#ffffff', cursor: 'pointer' };
+                return (
+                  <div {...getSuggestionItemProps(suggestion, { className, style })}>
+                    <span>{suggestion.description}</span>
+                  </div>
+                )
+              })}
+            </div>
+          </div>
+        )}
+      </PlacesAutocomplete>
         </Menu.Item>
 
         <Responsive as={Menu.Menu} position="right" minWidth={Responsive.onlyTablet.minWidth}>
